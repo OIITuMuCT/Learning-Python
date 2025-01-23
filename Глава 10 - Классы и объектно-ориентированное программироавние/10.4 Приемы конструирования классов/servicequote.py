a567c9b1c6975603b3286bac72b23d94ec792774
OIITuMuCT <vataman@live.ru>
2025-01-23 18:44:27 +0300

    Класс ServiceQuote, Car

--- Глава 10 - Классы и объектно-ориентированное программироавние/10.4 Приемы конструирования классов/servicequote.py
+++ Глава 10 - Классы и объектно-ориентированное программироавние/10.4 Приемы конструирования классов/servicequote.py
@@ -6,20 +6,20 @@ class ServiceQuote:
         self.__parts_charges = pcharge
         self.__labor_charges = lcharge
 
-    def set_part_charges(self, pcharge):
+    def set_parts_charges(self, pcharge):
         self.__parts_charges = pcharge
 
     def set_labor_charges(self, lcharge):
         self.__labor_charges = lcharge
     
-    def get_part_charges(self):
+    def get_parts_charges(self):
         return self.__parts_charges
     def get_labor_charges(self):
         return self.__labor_charges
     
     def get_sales_tax(self):
-        return __parts_charges * TAX_RATE
+        return self.__parts_charges * TAX_RATE
     
     def get_total_charges(self):
-        return __parts_charges + __labor_chsrges + \
-            (__parhts_charges * TAX_RATE)
+        return self.__parts_charges + self.__labor_charges + \
+            (self.__parts_charges * TAX_RATE)
