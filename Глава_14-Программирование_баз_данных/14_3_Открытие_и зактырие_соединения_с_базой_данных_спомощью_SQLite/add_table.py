65fa0bd7c9782e21fcc4a7e3c29c552b3681f12c
OIITuMuCT <vataman@live.ru>
2025-01-28 19:48:42 +0300

    multiple_tables

---
+++ Глава_14-Программирование_баз_данных/14_3_Открытие_и зактырие_соединения_с_базой_данных_спомощью_SQLite/add_table.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+def main():
+    # Подсоединится к базе данных.
+    conn = sqlite3.connect('inventory.db')
+    
+    # Получить курсор.
+    cur = conn.cursor()
+
+    # Добавить таблицу Inventory.
+    cur.execute('''CREATE TABLE Inventory (ItemId, INTEGER PRIMARY KEY NOT NULL,
+                ItemName TEXT, Price REAL)''')
+    # зафиксировать изменения.
+    conn.commit()
+    
+    # Закрыть соединение.
+    conn.close()
+
+if __name__ == '__main__':
+    main()
---
+++ Глава_14-Программирование_баз_данных/14_3_Открытие_и зактырие_соединения_с_базой_данных_спомощью_SQLite/multiple_tables.py
@@ -0,0 +1,28 @@
+
+
+import sqlite3
+
+def main():
+    # Присоединиться к базе данных.
+    conn = sqlite3.connect('company.db')
+    
+    # Получить курсор.
+    cur = conn.cursor()
+
+    # Добавить таблицу Customers.
+    cur.execute("""CREATE TABLE Customers (
+        CustomerID INTEGER PRIMARY KEY NOT NULL,
+        Name TEXT,
+        Email TEXT)""")
+    # Добавить таблицу Employees.
+    cur.execute(""" CREATE TABLE Employees (EmployeesID INTEGER PRIMARY KEY NOT NULL,
+                Name TEXT,
+                Position TEXT) """)
+    # Зафиксировать изменения.
+    conn.commit()
+    
+    # Закрыть соединение.
+    conn.close()
+
+if __name__ == '__main__':
+    main()
