e5a64e21c73f4ae3300d0239de41bb41a3ed9ae9
OIITuMuCT <vataman@live.ru>
2025-01-31 17:22:25 +0300

    employee_details setup_listbox

--- Глава_14-Программирование_баз_данных/14_11_Реляционные_данные/employee_details.py
+++ Глава_14-Программирование_баз_данных/14_11_Реляционные_данные/employee_details.py
@@ -23,3 +23,30 @@ class EmployeeDetails:
         
         # Создать кнопку выйти.
         self.__create_quit_button()
+
+    # Создать надпись с подсказкой для пользователя
+    def __create_prompt_label(self):
+        self.employee_prompt_label = tkinter.Label(
+            self.main_window, text= 'Выберите сотрудника'
+        )
+        self.employee_prompt_label.pack(side='top', padx=5, pady=5)
+    # Скомпоновать рамку, содержащую виджеты Listbox и Scrollbar.
+    def __build_listbox_frame(self):
+        # создать рамку для виджетов Listbox Scrollbar.
+        self.listbox_frame = tkinter.Frame(self.main_window)
+        
+        # Настроить виджет Listbox.
+        self.__setup_listbox()
+        
+        # Создать полосу прокрутки для просмотра элементов в виджете Listbox
+        self.__create_scrollBar()
+        
+        # Заполнить виджет Listbox именами сотрудников. 
+        self.__populate_listbox()
+
+        # Упаковать рамку виджета Listbox
+        self.listbox_frame.pack()
+
+    # Создать виджет Listbox для вывода имен сотрудников на экран
+    def __setup_listbox(self):
+        
